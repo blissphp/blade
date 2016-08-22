a3660958ac311dcf1a11fda63902bb2dcb71f2b4
Yury Plashenkov <plashenkov@gmail.com>
2016-08-23 03:02:52 +0600

    Fix

--- src/Blade.php
+++ src/Blade.php
@@ -12,12 +12,12 @@ class Blade extends Plugin
     {
         $serviceName = $serviceName ?: 'blade';
 
-        $container[$serviceName] = function ($c) {
+        $container[$serviceName] = function ($c) use ($options) {
             $blade = new BladeView($options['viewPaths'], $options['cachePath']);
             $blade['flash'] = $c->flash;
 
             return $blade;
-        }
+        };
 
         $container->response->setView($container->$serviceName);
     }
